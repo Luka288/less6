790345233e50cdd97751695422a8817d5eace65c
Luka <lukagaxokidze28@gmail.com>
2024-10-06 23:12:42 +0400

    less6

---
+++ main.js
@@ -0,0 +1,81 @@
+// responsive header
+const buttonContaier = document.querySelector(".nav-items");
+const burger = document.querySelector(".bars");
+const icon = document.querySelector(".fa-bars");
+
+let ul = document.createElement("ul");
+let Homeli = document.createElement("li");
+let Aboutli = document.createElement("li");
+let navHome = document.createElement("a");
+let navAbout = document.createElement("a");
+let closeIcon = document.createElement("i");
+closeIcon.classList.add("fa-solid", "fa-close");
+
+navHome.innerText = "home";
+navAbout.innerText = "about";
+
+ul.appendChild(closeIcon);
+ul.appendChild(Homeli);
+ul.appendChild(Aboutli);
+ul.classList.add("box");
+Homeli.appendChild(navHome);
+Aboutli.appendChild(navAbout);
+
+burger.appendChild(ul);
+
+icon.addEventListener("click", function () {
+  ul.classList.toggle("show");
+  if (ul.classList.contains("show")) {
+    ul.style.display = "flex";
+    icon.style.display = "none";
+    closeIcon.style.display = "block";
+  } else {
+    icon.syle.display = "block";
+    closeIcon.style.display = "none";
+    ul.style.display = "none";
+  }
+});
+
+closeIcon.addEventListener("click", function () {
+  ul.classList.remove("show");
+  icon.style.display = "block";
+  ul.style.display = "none";
+  closeIcon.style.display = "none";
+});
+
+// todo list
+let inputElementNew = document.getElementById("input-element");
+let btnElemetnNew = document.getElementById("add-btn");
+let ulResult = document.getElementById("ul-element");
+let clearAll = document.getElementById("clearAll");
+
+btnElemetnNew.addEventListener("click", function () {
+  // 1. get input field value
+  let inputValue = inputElementNew.value;
+
+  // 2.value -> li -> ul
+  let li = document.createElement("li");
+
+  //! check if input is empty
+  if (inputValue === "") {
+    alert("input is empty");
+    return;
+  }
+
+  let btnDelete = document.createElement("i");
+  btnDelete.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  btnDelete.addEventListener("click", function () {
+    li.remove();
+  });
+
+  li.textContent = inputValue;
+  li.appendChild(btnDelete);
+  ulResult.appendChild(li);
+
+  // 3.clear inout field value
+  inputElementNew.value = "";
+});
+
+clearAll.addEventListener("click", function () {
+  ulResult.innerHTML = " ";
+});
